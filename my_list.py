c2a7590cf8989df11815389d84bf46ff4a254655
Preet <patelpreet19800@gmail.com>
2026-06-29 17:34:20 +0530

    my_list.py

---
+++ my_list.py
@@ -0,0 +1,28 @@
+print("program 1")
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+print("--------------")
+
+print("program 2")
+
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits[1] = "Orange"
+
+print(fruits)
+
+
+print("-------------")
+
+print("program 3")
+
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits[2] = "Pomegranate"
+
+print(fruits)
