fc5ab6080657b39410b67d88675dcf1fd069e156
Preet <patelpreet19800@gmail.com>
2026-06-29 16:34:57 +0530

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+print("Progrsm 1")
+for i in range(5):
+    print("Preet")
+
+print("---------------")
+
+print("Program 2")
+number = int(input("Enter a number"))
+
+for i in range(1, 11):
+    print(number, "X", i, "=", number * i)
